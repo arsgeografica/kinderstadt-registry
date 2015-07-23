7e2b1651213852574b15ccd8f01f52452aa91ca6
Christian Wygoda <info@wygoda.net>
2015-07-23 18:35:46 +0200

    Cleaned up some DB migration steps

--- registry/migrations/versions/23dcbdfd33b5_passport_data_changes.py
+++ registry/migrations/versions/23dcbdfd33b5_passport_data_changes.py
@@ -8,7 +8,6 @@ Create Date: 2015-07-13 21:58:26.843809
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = '23dcbdfd33b5'
 down_revision = '8a00d265ee2'
