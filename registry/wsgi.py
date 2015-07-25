22cfcd9bf37961879a6e4dadc1ff1f52dbccec1e
Christian Wygoda <info@wygoda.net>
2015-07-25 19:29:37 +0200

    Last minute changes for deployment

---
+++ registry/wsgi.py
@@ -0,0 +1,8 @@
+import os
+from passgen.app import factory
+
+
+if 'REGISTRY_CONFIG_MODULE' not in os.environ:
+    raise RuntimeError('No REGISTRY_CONFIG_MODULE environment set.')
+
+application = factory(os.environ['REGISTRY_CONFIG_MODULE'])
